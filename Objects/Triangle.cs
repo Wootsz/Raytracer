9b7b27f35237696c4bd3078592909f89bc5e8b6d
Wootsz <wouterbrozius@hotmail.com>
2016-05-27 19:32:03 +0200

    Meer basic stuff

---
+++ Objects/Triangle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+
+namespace template.Objects
+{
+    class Triangle
+    {
+        public Vector3 p0, p1, p2, p3;         //triangle hoeken posities
+    }
+}
--- Raytracer.cs
+++ Raytracer.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Template;
+using OpenTK;
 
 namespace template
 {
@@ -10,16 +12,31 @@ namespace template
     {
         public Scene scene;
         public Camera camera;
+        public Surface screen;
+        public int screenWidth = 512, screenHeight = 512;
 
-        public Raytracer()
+        public Raytracer(Surface screen)
         {
-
+            this.screen = screen;
+            camera = new Camera();
+            scene = new Scene();
         }
 
         public void Render()
         {
-            //for alle pixels{ Ray = new Ray}
-            //Plot(pixelposition,color)
+            //Ray ray;
+            //voor alle pixels
+            for (int x = 0; x < screenWidth; x++)
+            {
+                for (int y = 0; y < screenHeight; y++)
+                {
+                    //ray = new Ray();
+                    //ray.position = new Vector3(camera.position)
+                    float color = 0; //=rayintersection.color;
+                    screen.Plot(x, y, (int)color);
+                }
+            }
         }
+
     }
 }
